642172e2758b3b5c737fcc91f58ba40e6bdcf162
Steven Dowd <swdowd@gmail.com>
2023-08-11 17:19:23 -0400

    Implemented canConstructWord and averagePair

--- prob-freq-pointers/average-pair.js
+++ prob-freq-pointers/average-pair.js
@@ -1,4 +1,41 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/*input: array of numbers nums, target value number
+output: boolean
+initialize a pointer at each end of the array 0, length -1
+average the values at each pointer, check if they match the target
+  if they do, return true
+if result is too large, move end pointer left
+if result is too small, move start pointer right
+if left pointer is greater than or equal to right pointer, exit while
+loop
+
+at that point, return false
+*/
+/**
+ * Accepts an array of numbers and a target average. Returns true
+ * if two elements of the array can average to the target. Returns
+ * false otherwise.
+ * @param {*} nums An array of numbers
+ * @param {*} targetAvg A number value
+ * @returns A boolean value
+ */
+function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (right > left) {
+    const currentAvg = (nums[left] + nums[right]) / 2;
+    if (currentAvg === targetAvg) {
+      return true;
+    }
+    if (currentAvg > targetAvg) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return false;
 }
+
 
--- prob-freq-pointers/can-construct-word.js
+++ prob-freq-pointers/can-construct-word.js
@@ -1,3 +1,53 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/*
+inputs:Two strings, word and letters
+outputs: boolean, true or false
+1st step, build freq counter for each input (wordfreq, lettersFreq)
+2nd step: compare the freq counters
+  for each key in wordFreq, check for it in lettersFreq
+    if not, return false
+  if that key is present, make sure the value in wordFreq is
+  less than or equal to the value in lettersFreq
+    if greater, return false
+
+  if we have not returned false by now, return true
+*/
+/**
+ * Accepts a target word and a collection of letters. Returns true
+ * if the word can be constructed from the given letters. Returns false
+ * otherwise.
+ * @param {*} word A string representing a word
+ * @param {*} letters A string representign letters available for use
+ * @returns A boolean value
+ */
+function canConstructWord(word, letters) {
+  const wordFreq = buildFreqCounter(word);
+  const letterFreq = buildFreqCounter(letters);
+
+  for (let key in wordFreq) {
+    if (!(key in letterFreq)) {
+      return false;
+    }
+    if (wordFreq[key] > letterFreq[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * Builds a frequency counter object from an iterable.
+ *
+ *
+ */
+function buildFreqCounter(item) {
+  const freqCounter = {};
+  for (let i = 0; i < item.length; i++) {
+    const current = freqCounter[item[i]] || 0;
+    freqCounter[item[i]] = current + 1;
+  }
+  return freqCounter;
 }
